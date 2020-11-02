7a94d090af7449eb8916203997630e25b67fd28f
Pinwhee1 <ezexit00@gmail.com>
2020-11-01 19:16:35 -0500

    Updated readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Simple lil password gen with character exclusion and customizable length.
-
+## Requires "pyperclip" module installed. (install with `pip install pyperclip`)
 ![What it looks like](new_pwgen.png)
--- __main__.py
+++ __main__.py
@@ -27,7 +27,7 @@ class SubWindow(tk.Toplevel):
 class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
-        
+
         # Window
         self.geometry("600x300+630+200")
         self.resizable(False, False)
